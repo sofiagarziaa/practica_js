981d965338686fb1364a0444b96c6daeb749fca4
sofiagarziaa <sgarzia@udesa.edu.ar>
2024-05-16 17:19:51 -0300

    ejercio 2

--- js/index.js
+++ js/index.js
@@ -1,10 +1,21 @@
 //ejercicio 1:
+//let misDatos = {
+//    nombre: "Sofia",
+//    apellido:"Garzia",
+//    dni:"45906343",
+//   comidasFavoritas:["Empanada", "pizza", "milanesa"] //array
+//};
+
+//ejercicio 2:
 let misDatos = {
     nombre: "Sofia",
     apellido:"Garzia",
+    edad: "19",
     dni:"45906343",
-    comidasFavoritas:["Empanada", "pizza", "milanesa"] //array
+    comidasFavoritas:["Empanada", "pizza", "milanesa"],
+    saludar: function() {
+        return "Hola mi nombre es ${this.nombre} ${this.apellido} y tengo ${this.edad} "
+    }
 };
 
-
-
+console.log(misDatos.saludar());
