48515087b872476fb9e1a48e054e882ca8bfab7a
sofiagarziaa <sgarzia@udesa.edu.ar>
2024-06-07 01:35:14 -0300

    fetch terminado

--- fetch/index.js
+++ fetch/index.js
@@ -1,23 +1,29 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const characterList = document.querySelector(".characterList");
+document.addEventListener('DOMContentLoaded', function () {
+    const characterList = document.querySelector('.characterList');
 
-    fetch("https://rickandmortyapi.com/api/character")
-    .then(response => response.json())
-    .then(data => {
-        
-        let characters = '';
+    fetch('https://rickandmortyapi.com/api/character')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            let characters = '';
 
-        data.results.forEach(character => {
-            characters += `
-                <article>
-                    <img src="${character.image}" alt="${character.name}">
-                    <p>Name: ${character.name}</p>
-                    <p>Status: ${character.status}</p>
-                </article>
-            `;
+            data.results.forEach(function (character) {
+                characters += `
+                    <article class="elemento-hijo">
+                        <img class="img" src="${character.image}" alt="${character.name}">
+                        <div class="info">
+                            <h3>${character.name}</h3>
+                            <p>Status: ${character.status}</p>
+                            <p>Species: ${character.species}</p>
+                        </div>
+                    </article>
+                `;
+            });
+
+            characterList.innerHTML = characters;
+        })
+        .catch(function (error) {
+            console.log('Error fetching data:', error);
         });
-
-        characterList.innerHTML = characters;
-    })
-    .catch(error => console.error("Error fetching characters:", error));
 });
